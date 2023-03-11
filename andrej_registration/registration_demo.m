451d6d32dff4191d0ba2b67c88a238a469d249e0
andrejsho <77636516+andrejsho@users.noreply.github.com>
2023-03-11 10:46:55 +0100

    Registration in one function including oprimizer
    The registration happens now in one function. For the first time an optimizer is used to minimize the distances while rotating. Also instead of making both ribs to point along the z axis now one rib stays always fixed and only the other one is moved.

---
+++ andrej_registration/registration_demo.m
@@ -0,0 +1,16 @@
+clear;
+load('rib1_skel_end2.mat')
+load('rib2_skel_end2.mat')
+
+line_moving=volume2line(rib_right);
+line_fixed=volume2line(rib_left);
+
+line_registered = round(MyRegistration(line_fixed,line_moving));
+
+
+vol_registered=zeros(size(rib_left));
+for i=1:size(line_registered,1)
+    vol_registered(line_registered(i,1),line_registered(i,2),line_registered(i,3))=1;
+end
+
+volshow(vol_registered+double(rib_left))
