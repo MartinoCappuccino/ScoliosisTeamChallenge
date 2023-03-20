b88463d3cf666687808db1582d0006c1bb1964cb
MartinoCappuccino <martijnstoorvogel@gmail.com>
2023-03-20 17:51:07 +0100

    bwskel of ribs voxel data

--- main.m
+++ main.m
@@ -1,42 +1,36 @@
-clear all;
-close all;
-
-image=niftiread("..\data\Scoliose\1preop.nii");
-
-ribcage=uint8(get_ribcage(image, 5, 3, 1350));
-
-pcribcage = voxel_to_pointcloud(ribcage, [255, 0, 0]);
-
-[pcspine, pcribs] = get_points(ribcage, colorspine=[0 255 0], colorribs=[0 0 255]);
-
-labels = cluster_ribs(pcribs);
-
-% Color code the point cloud based on the labels
-figure;
-colormap = hsv(max(labels));
-colors = colormap(labels,:);
-pcshow(pcribs.Location, colors);
-axis equal;
-
-% Generate separate point clouds for each label
-for i = 1:max(labels)
-    figure;
-    label_points = pcribs.Location(labels == i,:);
-    pcshow(label_points, colormap(i,:));
-    axis equal;
-    title(sprintf('Label %d', i));
-end
-
-%% PLOT
-figure;
-pcshow(pcribs)
-hold on;
-pcshow(pcspine)
-pcshow(pcribcage)
-set(gcf,'color','w');
-set(gca,'color','w');
-xlabel('X');
-ylabel('Y');
-zlabel('Z');
-legend('Spine', 'Ribs', 'Ribcage');ni
+function main(filename)
+    %% Read image
+    image=niftiread(filename);
+    image=imresize3(image, [512, 512, 437]);
     
+    %% Extract ribcage
+    ribcage=uint8(get_ribcage(image, 5, 3, 1350));
+    pcribcage = voxel_to_pointcloud(ribcage, [255, 0, 0]);
+    
+    %% Extract spine and ribs
+    [pcspine, pcribs, spine, ribs] = get_points(ribcage, [0, 255, 0], [0, 0, 255]);
+    
+    %% Extract individual ribs
+    % labels = cluster_ribs(pcribs);
+
+    %% Registration of ribs
+
+    %% Calculation of deformity
+    
+    %% PLOT
+    volshow(ribcage)
+    volshow(ribs)
+    volshow(spine)
+    %% PLOT pointclouds
+    figure; hold on;
+    pcshow(pcribs)
+    pcshow(pcspine)
+    pcshow(pcribcage)
+    set(gcf,'color','w');
+    set(gca,'color','w');
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Z');
+    legend('Spine', 'Ribs', 'Ribcage');
+
+end
--- voxel_to_pointcloud.m
+++ voxel_to_pointcloud.m
@@ -3,9 +3,9 @@ function [pointcloud] = voxel_to_pointcloud(image, color)
     pointcloud = pointCloud([x(:),y(:),z(:)]);
     
     pointscolor=uint8(zeros(pointcloud.Count,3));
-    pointscolor(:,1)=color(0);
-    pointscolor(:,2)=color(1);
-    pointscolor(:,3)=color(2);
+    pointscolor(:,1)=color(1);
+    pointscolor(:,2)=color(2);
+    pointscolor(:,3)=color(3);
     
     pointcloud.Color=pointscolor;
 end
